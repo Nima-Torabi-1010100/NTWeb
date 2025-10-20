e8f857c7141a8a65e50325c7a129a511e7c6fc6e
Nima-Torabi-1010100 <nimatorabi.t@gmail.com>
2025-10-20 10:21:50 +0330

    feat(quick-access): add quick access to mobile view home page

--- NTWEB/Models/Service/ServiceForm.cs
+++ NTWEB/Models/Service/ServiceForm.cs
@@ -5,16 +5,20 @@ namespace NTWEB.Models.Service
 {
     public class ServiceForm : FormBase
     {
+        [Required(ErrorMessage = ValidationMessage.IsRequired)]
         public string PhoneNumber { get; set; }
         public string ProjectName { get; set; }
         [Required(ErrorMessage = ValidationMessage.IsRequired)]
         public string ProjectType { get; set; }
+        [Required(ErrorMessage = ValidationMessage.IsRequired)]
         public string CompanyName { get; set; }
+        [Required(ErrorMessage = ValidationMessage.IsRequired)]
         public int Budget { get; set; }
         [Required(ErrorMessage = ValidationMessage.IsRequired)]
         public string PaymentMethod { get; set; }
         [Required(ErrorMessage = ValidationMessage.IsRequired)]
         public string Country { get; set; }
+        [Required(ErrorMessage = ValidationMessage.IsRequired)]
         public string City { get; set; }
         public string Location { get; set; }
     }
